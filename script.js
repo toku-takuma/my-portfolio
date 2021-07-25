1a89f82c5a2da3f2ce3945a0afa29b475fd3ec7a
toku-takuma <tokutaku1412@gmail.com>
2021-07-25 17:43:41 +0900

    自己紹介サイトを作りました

--- script.js
+++ script.js
@@ -1,50 +1,43 @@
-// PhotoSwipe
-// initPhotoSwipeFromDOM(".js-my-gallery");
+  //iOS対策
+  //iOSでは疑似要素を含むaタグのリンクは２回タップしないと遷移とページ内スクロールをしないため、
+  //ユーザーエージェント判定でiOSの場合はbodyタグにiosを付与し、対象の疑似要素をdisplay: noneする
+  let ua = navigator.userAgent;
+  if (/iPhone|iPad|iPod/.test(ua)) {
+    ("body").addClass("ios");
+  }
 
-(function () {
-  //Productsのリンクを有効化
-  (".works-url").on("click", "a", function (e) {
-    e.stopPropagation();
+
+  $(function () {
+
+    //ページ内スクロール
+    var navHeight = $(".header").outerHeight();
+  
+    $('a[href^="#"]').on("click", function () {
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? "html" : href);
+      var position = target.offset().top - navHeight;
+      $("html, body").animate({ scrollTop: position, }, 300, "swing");
+      return false;
+    });
+  
+    //ページトップ
+    $("#js-page-top").on("click", function () {
+      $("body,html").animate({ scrollTop: 0, }, 300);
+      return false;
+    });
+  
   });
-
-  //ページ内スクロール
-  var nav = (".gnavh");
-  var navHeight = nav.outerHeight();
-
-  ('a[href^="#"]').on("click", function () {
-    var href = (this).attr("href");
-    var target = (href == "#" || href == "" ? "html" : href);
-    var position = target.offset().top - navHeight;
-    ("html, body").animate(
-      {
-        scrollTop: position,
-      },
-      300,
-      "swing"
-    );
-    return false;
-  });
-
-//メニュー
-(".openbtn1").click(function () {
-  (this).toggleClass('active');
-    (".gnav").toggleClass('panelactive');
-
-(".gnav a").click(function () {
-    (".openbtn1").removeClass('active');
-    (".gnav").removeClass('panelactive');
-
+  
 function showElementAnimation() {
-  var element = document.getElementsByClassName("js-animation");
+  let element = document.getElementsByClassName("js-animation");
   if (!element) return; // 要素がなかったら処理をキャンセル
-​
-  var showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
-  var scrollY = window.pageYOffset;
-  var windowH = window.innerHeight;
-​
-  for (var i = 0; i < element.length; i++) {
-    var elemClientRect = element[i].getBoundingClientRect();
-    var elemY = scrollY + elemClientRect.top;
+  let showTiming = window.innerHeight > 768 ? 200 : 40; // 要素が出てくるタイミングはここで調整
+  let scrollY = window.pageYOffset;
+  let windowH = window.innerHeight;
+
+  for (let i = 0; i < element.length; i++) {
+    let elemClientRect = element[i].getBoundingClientRect();
+    let elemY = scrollY + elemClientRect.top;
     if (scrollY + windowH - showTiming > elemY) {
       element[i].classList.add("is-show");
     } else if (scrollY + windowH < elemY) {
